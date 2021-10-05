35368a65d348cbef06046996117e575a40dcf0c4
Remy Suarez <91926310+suarezmr@users.noreply.github.com>
2021-10-04 22:16:55 -0400

    Add files via upload

--- Pre-Project Group Assignment.R
+++ Pre-Project Group Assignment.R
@@ -1,8 +1,8 @@
-#Set up working directory and read CSV file into R as a data frame.
-setwd("/Users/remington/Downloads")
-read.csv("Flinfo2021.csv")
-mydata <- read.csv("Flinfo2021.csv")
-summary(mydata)
+#Read CSV files into R.
+read.csv("https://raw.githubusercontent.com/suarezmr/Raven-Analytics-Practicum/main/flinfo2021.csv")
+#Turn CSV into data frame
+FL2021 <- read.csv("https://raw.githubusercontent.com/suarezmr/Raven-Analytics-Practicum/main/flinfo2021.csv")
+summary(FL2021)
 #Import libraries to be used
 library(car)
 library(psych)
@@ -11,18 +11,17 @@ library(lmtest)
 library(nortest)
 library(dplyr)
 #Column number sequence vector
-colNum <- (seq_len(ncol(mydata)))
+colNum <- (seq_len(ncol(FL2021)))
 #Turn column vector into a data frame.
 names_df <- as.data.frame(colNum)
 #Add column Name column to names_df data frame.
-names_df$colname <- (colnames(mydata))
+names_df$colname <- (colnames(FL2021))
 #Add variable type to names_df data frame as a column.
-names_df$type <- (sapply(mydata, class))
+names_df$type <- (sapply(FL2021, class))
 View(names_df)
 #Change variable types as needed.
-mydata$population <- as.numeric(mydata$population)
-mydata$Deaths <- as.numeric(mydata$population)
-mydata$Confirmed <- as.numeric(mydata$Confirmed)
+FL2021$population <- as.numeric(FL2021$population)
+FL2021$Deaths <- as.numeric(FL2021$Deaths)
 #Change column names as needed.
-mydata <- mydata %>% rename(County = Admin2)
-View(mydata)
+FL2021 <- FL2021 %>% rename(County = Admin2)
+View(FL2021)
